1cea8f3f86bf2993266affeb93eebf6bc598c932
Pierre Marchand <pierre.jimthefrench@gmail.com>
2017-07-05 17:10:04 +0200

    working on hpddm

--- test/functional_tests/hpddm_tests/hpddm_tests.cpp
+++ test/functional_tests/hpddm_tests/hpddm_tests.cpp
@@ -1,6 +1,7 @@
 #include <htool/wrapper_hpddm.hpp>
 #include <htool/fullACA.hpp>
-
+#include <htool/matrix.hpp>
+// #include <htool/loading.hpp>
 
 
 using namespace std;
@@ -29,7 +30,7 @@ public:
 };
 
 
-int main(){
+int main(int argc, char const *argv[]){
 
 	// Initialize the MPI environment
 	MPI_Init(NULL, NULL);
@@ -46,12 +47,12 @@ int main(){
 	bool test = 0;
 	const int ndistance = 4;
 	double distance[ndistance];
-	distance[0] = 10; distance[1] = 20; distance[2] = 30; distance[3] = 40;
+	distance[0] = 3; distance[1] = 5; distance[2] = 7; distance[3] = 10;
 	SetNdofPerElt(1);
 	SetEpsilon(1e-8);
-	SetEta(-0.1);
+	SetEta(0.1);
 
-	for(int idist=0; idist<ndistance; idist++)
+	for(int idist=0; idist<1; idist++)
 	{
 		// cout << "Distance between the clusters: " << distance[idist] << endl;
 
@@ -59,8 +60,8 @@ int main(){
 		// we set a constant seed for rand because we want always the same result if we run the check many times
 		// (two different initializations with the same seed will generate the same succession of results in the subsequent calls to rand)
 
-		int nr = 10;
-		int nc = 10;
+		int nr = 10000;
+		int nc = 10000;
 		vector<int> Ir(nr); // row indices for the lrmatrix
 		vector<int> Ic(nc); // column indices for the lrmatrix
 
@@ -87,30 +88,86 @@ int main(){
 			tab2[j]=j;
 		}
 
+		HPDDM::Option& opt = *HPDDM::Option::get();
+    opt.parse(argc, argv, rank == 0);
+		if(rank != 0)
+        opt.remove("verbosity");
 		MyMatrix A(p1,p2);
 		HMatrix<fullACA,double> HA(A,p1,tab1,p2,tab2);
-		std::vector<double> x_ref(nc,1),x_test(nc,1),f(nr,1);
-    // f = HA*x_ref;
-    cout << "f "<<f<<std::endl;
-    HPDDMOperator<fullACA,double> A_HPDDM(HA);
-    double* const rhs = &(f[0]);
-    double* x = &(x_test[0]);
+		int nbr_dmat = HA.get_ndmat();
+		int nbr_lmat = HA.get_nlrmat();
+		int comp = HA.compression();
+
+		if (rank==0){
+			cout << "nbr_dmat : "<<nbr_dmat<<endl;
+			cout << "nbr_lmat : "<<nbr_lmat<<endl;
+			cout << "compression : "<<comp<<endl;
+		}
+
+		std::vector<double> x_ref(nc,1),f(nc,1),x_test_global(nc,1);
+		vector<R3> p1_local(nc),p2_local(nc);
+		f=A*x_ref;
+		nc=HA.get_local_size_cluster();
+		const std::vector<std::vector<int>>& MasterClusters = HA.get_MasterClusters();
+		std::vector<double> x_test_local(nc,0),f_local(nc,1);
+		for (int i=0;i<nc;i++){
+			f_local[i]=f[MasterClusters[rank][i]];
+			p1_local[i]=p1[MasterClusters[rank][i]];
+			p2_local[i]=p2[MasterClusters[rank][i]];
+		}
+		// MyMatrix A_local(p1,p2_local);
+    // f = A*x_ref;
+
+
+		// cout << pow(norm2(fl),2)<<endl;
+
+		// for (int i=0; i< HA.MasterClusters[rank].size(); i++) {
+		// 	x_refl[i] -= x_ref[HA.MasterClusters[rank][i]];
+		// }
+		// cout <<"rank : "<<rank<<" "<< norm2(x_refl)<<endl;
+		// double erreur2 = norm2(HA*x_ref-f)/norm2(f);
+		// if (rank==0){
+		// 	cout <<"error on mat vec prod : "<<erreur2 << endl;
+		// }
+		//
+    HPDDMOperator<fullACA,double> A_HPDDM(HA,A);
+    double* const rhs = &(f_local[0]);
+    double* x = &(x_test_local[0]);
+		double time = MPI_Wtime();
     HPDDM::IterativeMethod::solve(A_HPDDM, rhs, x, 1,HA.get_comm());
-    cout << "x_test "<<x_test<<endl;
-    cout << "x_ref  "<<x_ref<<endl;
-		double erreur2 = norm2(A*x_ref-f);
-    double inv_erreur2 = norm2(x_ref-x_test);
-		// double erreurFrob = Frobenius_absolute_error(HA,A);
-		// double compression = HA.compression();
-		// int nb_lrmat = HA.get_nlrmat();
-		// int nb_dmat  = HA.get_ndmat();
-    // if (rank==0){
-    //   cout <<"error on mat vec prod : "<<erreur2 << endl;
-    //   cout <<"error on inversion : "<<inv_erreur2 << endl;
-    //
-    // }
-		// test = test || !(erreurFrob<GetEpsilon()*10);
-		// test = test || !(erreur2<GetEpsilon()*100);
+		if (rank==0){
+			cout << MPI_Wtime() - time<<endl;
+		}
+		std::vector<double> rcv;
+		rcv.resize(nr);
+
+		std::vector<int> recvcounts(size);
+		std::vector<int>  displs(size);
+
+		displs[0] = 0;
+
+		for (int i=0; i<size; i++) {
+			recvcounts[i] = MasterClusters[i].size();
+			if (i > 0)
+				displs[i] = displs[i-1] + recvcounts[i-1];
+		}
+
+
+		MPI_Allgatherv(&(x_test_local.front()), recvcounts[rank], MPI_DOUBLE, &(rcv.front()), &(recvcounts.front()), &(displs.front()), MPI_DOUBLE, HA.get_comm());
+
+		for (int i=0; i<size; i++)
+		for (int j=0; j< MasterClusters[i].size(); j++)
+			x_test_global[MasterClusters[i][j]] = rcv[displs[i]+j];
+
+
+
+		double inv_erreur2_global=norm2(f-A*x_test_global)/norm2(f);
+
+    if (rank==0){
+      cout <<"error on inversion : "<<inv_erreur2_global << endl;
+
+    }
+		// test = test || !(inv_erreur2<1.e-6); // default tol in hpddm
 
 	}
 	// Finalize the MPI environment.
