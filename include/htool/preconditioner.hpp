aef6951b107ac517a8a07550be56a219671d40f1
Pierre Marchand <pierre.jimthefrench@gmail.com>
2017-09-15 18:14:48 +0200

    working on hpddmw wrapper and precond

--- include/htool/preconditioner.hpp
+++ include/htool/preconditioner.hpp
@@ -4,12 +4,13 @@
 #include "matrix.hpp"
 #include "lapack.hpp"
 #include "wrapper_mpi.hpp"
+#include "wrapper_hpddm.hpp"
 
 namespace htool{
 
 
-template<typename T>
-class Preconditioner{
+template<template<typename> class LowRankMatrix, typename T>
+class DDM{
 private:
 
   int n;
@@ -17,50 +18,57 @@ private:
   const std::vector<int> neighbors;
   // const std::vector<int> cluster_to_ovr_subdomain;
   std::vector<std::vector<int> > intersections;
-  std::vector<T> fact;
   std::vector<T> vec_ovr;
   std::vector<std::vector<T>> snd,rcv;
+  HPDDMDense<LowRankMatrix,T> hpddm_op;
+  std::vector<T> mat_loc;
+  std::vector<double> D;
   MPI_Comm comm;
 
 
 
-  void synchronize(bool sum){
-
-    fill(vec_ovr.begin()+n_inside,vec_ovr.end(),0);
-    for (int i=0;i<neighbors.size();i++){
-      for (int j=0;j<intersections[i].size();j++){
-        snd[i][j]=vec_ovr[intersections[i][j]];
-        // if (!sum && intersections[i][j]>=n_inside)  snd[i][j]=0;
-      }
-    }
-
-    // Communications
-		std::vector<MPI_Request> rq(2*neighbors.size());
-
-    for (int i=0;i<neighbors.size();i++){
-
-      MPI_Isend( snd[i].data(), snd[i].size(), wrapper_mpi<T>::mpi_type(), neighbors[i], 0, comm, &(rq[i]));
-
-		  MPI_Irecv( rcv[i].data(), rcv[i].size(), wrapper_mpi<T>::mpi_type(), neighbors[i], MPI_ANY_TAG, comm, &(rq[i+neighbors.size()]));
-    }
-    MPI_Waitall(rq.size(),rq.data(),MPI_STATUSES_IGNORE);
-
-    for (int i=0;i<neighbors.size();i++){
-      for (int j=0;j<intersections[i].size();j++){
-        // if (sum){
-          vec_ovr[intersections[i][j]]+=rcv[i][j];
-        // }
-        // else {//if (intersections[i][j]>=n_inside){
-        //   vec_ovr[intersections[i][j]]+=rcv[i][j];
-        //   // if (rcv[i][j].real()==0) std::cout << "pouet "<< std::endl;
-        // }
-      }
-    }
-
-  }
+  // void synchronize(bool sum){
+  //
+  //   fill(vec_ovr.begin()+n_inside,vec_ovr.end(),0);
+  //   for (int i=0;i<neighbors.size();i++){
+  //     for (int j=0;j<intersections[i].size();j++){
+  //       snd[i][j]=vec_ovr[intersections[i][j]];
+  //       // if (!sum && intersections[i][j]>=n_inside)  snd[i][j]=0;
+  //     }
+  //   }
+  //
+  //   // Communications
+	// 	std::vector<MPI_Request> rq(2*neighbors.size());
+  //
+  //   for (int i=0;i<neighbors.size();i++){
+  //
+  //     MPI_Isend( snd[i].data(), snd[i].size(), wrapper_mpi<T>::mpi_type(), neighbors[i], 0, comm, &(rq[i]));
+  //
+	// 	  MPI_Irecv( rcv[i].data(), rcv[i].size(), wrapper_mpi<T>::mpi_type(), neighbors[i], MPI_ANY_TAG, comm, &(rq[i+neighbors.size()]));
+  //   }
+  //   MPI_Waitall(rq.size(),rq.data(),MPI_STATUSES_IGNORE);
+  //
+  //   for (int i=0;i<neighbors.size();i++){
+  //     for (int j=0;j<intersections[i].size();j++){
+  //       // if (sum){
+  //         vec_ovr[intersections[i][j]]+=rcv[i][j];
+  //       // }
+  //       // else {//if (intersections[i][j]>=n_inside){
+  //       //   vec_ovr[intersections[i][j]]+=rcv[i][j];
+  //       //   // if (rcv[i][j].real()==0) std::cout << "pouet "<< std::endl;
+  //       // }
+  //     }
+  //   }
+  //
+  // }
 
 public:
-  Preconditioner(const IMatrix<T>& mat0, const std::vector<int>&  ovr_subdomain_to_global0, const std::vector<int>& cluster_to_ovr_subdomain0, const std::vector<int>& neighbors0, const std::vector<std::vector<int> >& intersections0, MPI_Comm comm0=MPI_COMM_WORLD): n(ovr_subdomain_to_global0.size()), n_inside(cluster_to_ovr_subdomain0.size()), neighbors(neighbors0),fact(n*n), vec_ovr(n), comm(comm0) {
+  DDM(const IMatrix<T>& mat0, const HMatrix<LowRankMatrix,T>& hmat_0,
+    const std::vector<int>&  ovr_subdomain_to_global0,
+    const std::vector<int>& cluster_to_ovr_subdomain0,
+    const std::vector<int>& neighbors0,
+    const std::vector<std::vector<int> >& intersections0,
+    MPI_Comm comm0=MPI_COMM_WORLD): hpddm_op(hmat_0), n(ovr_subdomain_to_global0.size()), n_inside(cluster_to_ovr_subdomain0.size()), neighbors(neighbors0), vec_ovr(n),mat_loc(n*n), D(n), comm(comm0) {
 
     std::vector<int> renum(n,-1);
     std::vector<int> renum_to_global(n);
@@ -85,7 +93,6 @@ public:
     //     fact[renum[i]+renum[j]*n]=mat0.get_coef(ovr_subdomain_to_global0[i],ovr_subdomain_to_global0[j]);
     //   }
     // }
-    fact = mat0.get_submatrix(renum_to_global,renum_to_global).get_mat();
 
     intersections.resize(neighbors.size());
     for (int i=0;i<neighbors.size();i++){
@@ -103,36 +110,50 @@ public:
       rcv[i].resize(intersections[i].size());
     }
 
-  }
-  void num_fact(){
-    const char l='L';
-    int lda=n;
-    int info;
-    Lapack<T>::potrf(&l,&n,fact.data(),&lda,&info);
-  }
+    bool sym=false;
+    mat_loc= mat0.get_submatrix(renum_to_global,renum_to_global).get_mat();
+    hpddm_op.initialize(n, sym, mat_loc.data(), neighbors, intersections);
 
-  void apply(const T* const in, T* const out) {
+    fill(D.begin(),D.begin()+n_inside-1,1);
+    fill(D.begin()+n_inside,D.end(),0);
 
-    // Without overlap to with overlap
-    std::copy_n(in,n_inside,vec_ovr.data());
-    // std::fill(vec_ovr.begin(),vec_ovr.end(),0);
-    // std::fill_n(vec_ovr.begin(),n_inside,1);
-    synchronize(false);
-    // std::cout << vec_ovr<<std::endl;
-    //
-    const char l='L';
-    int lda=n;
-    int ldb=n;
-    int nrhs =1 ;
-    int info;
-
-    Lapack<T>::potrs(&l,&n,&nrhs,fact.data(),&lda,vec_ovr.data(),&ldb,&info);
-
-    //
-    synchronize(true);
-    std::copy_n(vec_ovr.data(),n_inside,out);
+    hpddm_op.super::initialize(D.data());
+    hpddm_op.callNumfact();
+    std::vector<T> x_ref(n,1),f_global(n,1);
+    T* const f = &(f_global[0]);
+    T* sol = &(x_ref[0]);
+    HPDDM::IterativeMethod::solve(hpddm_op, f, sol, 1,comm);
 
   }
+  // void num_fact(){
+  //   const char l='L';
+  //   int lda=n;
+  //   int info;
+  //   Lapack<T>::potrf(&l,&n,fact.data(),&lda,&info);
+  // }
+
+  // void apply(const T* const in, T* const out) {
+  //
+  //   // Without overlap to with overlap
+  //   std::copy_n(in,n_inside,vec_ovr.data());
+  //   // std::fill(vec_ovr.begin(),vec_ovr.end(),0);
+  //   // std::fill_n(vec_ovr.begin(),n_inside,1);
+  //   synchronize(false);
+  //   // std::cout << vec_ovr<<std::endl;
+  //   //
+  //   const char l='L';
+  //   int lda=n;
+  //   int ldb=n;
+  //   int nrhs =1 ;
+  //   int info;
+  //
+  //   Lapack<T>::potrs(&l,&n,&nrhs,fact.data(),&lda,vec_ovr.data(),&ldb,&info);
+  //
+  //   //
+  //   synchronize(true);
+  //   std::copy_n(vec_ovr.data(),n_inside,out);
+  //
+  // }
 
 
 
--- include/htool/wrapper_hpddm.hpp
+++ include/htool/wrapper_hpddm.hpp
@@ -1,33 +1,55 @@
-#ifndef HPDDM_CALL_H
-#define HPDDM_CALL_H
+#ifndef WRAPPER_HPDDM_HPP
+#define WRAPPER_HPDDM_HPP
 
 #define HPDDM_NUMBERING 'C'
 #include <HPDDM.hpp>
 #include "hmatrix.hpp"
 #include "matrix.hpp"
-#include "preconditioner.hpp"
+
 namespace htool{
 
 template< template<typename> class LowRankMatrix, typename T>
-struct HPDDMOperator : HPDDM::EmptyOperator<T> {
-  HMatrix<LowRankMatrix,T>& HA;
-  Preconditioner<T>& P;
-  HPDDMOperator(HMatrix<LowRankMatrix,T>& A,Preconditioner<T>& P0) : HPDDM::EmptyOperator<T>(A.get_local_size_cluster()), HA(A), P(P0) {}
+class HPDDMEmpty : public HPDDM::EmptyOperator<T> {
+  const HMatrix<LowRankMatrix,T>& HA;
+public:
+  HPDDMEmpty(const HMatrix<LowRankMatrix,T>& A) : HPDDM::EmptyOperator<T>(A.get_local_size_cluster()), HA(A) {}
   void GMV(const T* const in, T* const out, const int& mu = 1) const {
     HA.mvprod_local(in,out);
   }
   template<bool = true>
   void apply(const T* const in, T* const out, const unsigned short& mu = 1, T* = nullptr, const unsigned short& = 0) const {
-    // std::copy_n(in, this->_n, out);
-    P.apply(in,out);
-//     for(int i = 0; i < this->_n; ++i) {
-//   #if 1
-//   out[i] = in[i] / P.get_coef(i, i);
-//   #else
-//   out[i] = in[i];
-//   #endif
-// }
+    std::copy_n(in, this->_n, out);
   }
+};
+
+template< template<typename> class LowRankMatrix, typename T>
+void solve(const HMatrix<LowRankMatrix,T>& HA, std::vector<T>& x0,  const std::vector<T> &  rhs0){
+  HPDDMEmpty<LowRankMatrix,T> A_HPDDM(HA);
+  
+  HPDDM::IterativeMethod::solve(A_HPDDM, rhs0.data(), x0.data(), 1,HA.get_comm());
+}
+
+
+
+
+template< template<typename> class LowRankMatrix, typename T>
+class HPDDMDense : public HpDense<T> {
+private:
+  const HMatrix<LowRankMatrix,T>& HA;
+
+public:
+
+  HPDDMDense(const HMatrix<LowRankMatrix,T>& A):HA(A){}
+
+  void GMV(const T* const in, T* const out, const int& mu = 1) const {
+    std::cout << "àk" << std::endl;
+    HA.mvprod_local(in,out);
+    this->super::scaledExchange(out, mu);
+
+  }
+
+
+
 };
 
 
