0fb46308eb188a6f294707d4105e56b6d1c611be
Pierre Marchand <pierre.jimthefrench@gmail.com>
2018-08-20 15:48:26 +0200

    idem

--- include/htool/wrappers/wrapper_hpddm.hpp
+++ include/htool/wrappers/wrapper_hpddm.hpp
@@ -114,6 +114,11 @@ public:
         P.build_coarse_space(Mi,generator_Bi,x);
     }
 
+    void build_coarse_space(Matrix<T>& Mi, const std::vector<R3>& x){
+        // Coarse space
+        P.build_coarse_space(Mi,x);
+    }
+
     void facto_one_level(){
         P.facto_one_level();
     }
