43f572b0ad8190ec281e740d8b893844d9fad497
Pierre Marchand <pierre.jimthefrench@gmail.com>
2019-12-20 10:44:47 +0000

    Fixes #3 and clean scripts

--- tests/performance_tests/hmat.hpp
+++ tests/performance_tests/hmat.hpp
@@ -128,7 +128,7 @@ int hmat(int argc, char *argv[]){
 	}
 	HA.add_info("Mean_global_mat_vec_prod",NbrToStr(meanmean));
 	HA.add_info("Max_global_mat_vec_prod",NbrToStr(meanmax));
-	HA.save_infos((outputpath+"/"+outputfile).c_str(),,std::ios::app,": ");
+	HA.save_infos((outputpath+"/"+outputfile).c_str(),std::ios::app,": ");
 
 	// Finalize the MPI environment.
 	MPI_Finalize();
