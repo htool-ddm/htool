7939ebd7f48cf997a772129a7a8b344d21e00ca7
Pierre Marchand <pierre.jimthefrench@gmail.com>
2021-08-03 15:10:36 +0200

    remove deprecated gui, python interface and performance tests

--- include/htool/misc/define.hpp
+++ include/htool/misc/define.hpp
@@ -6,7 +6,7 @@
  *    HTOOL_VERSION       - Version of the framework.
  *    HTOOL_MKL           - If not set to zero, Intel MKL is chosen as the linear algebra backend. */
 
-#define HTOOL_VERSION "0.5.0"
+#define HTOOL_VERSION "0.6.0"
 #ifndef HTOOL_MKL
 #    ifdef INTEL_MKL_VERSION
 #        define HTOOL_MKL 1
--- tests/performance_tests/hmat_geometric_splitting_partialACA.cpp
+++
@@ -1,8 +0,0 @@
-#include "hmat.hpp"
-
-int main(int argc, char *argv[]) {
-
-    hmat<Cluster<PCARegularClustering>, partialACA>(argc, argv);
-
-    return 0;
-}
