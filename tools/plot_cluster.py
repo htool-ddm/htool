f39c0ca9bdb6715c5f52f2d0fa4867c8782f5a3a
Pierre Marchand <pierre.jimthefrench@gmail.com>
2024-09-17 17:56:44 +0200

    update python scripts

--- tools/plot_cluster.py
+++ tools/plot_cluster.py
@@ -1,25 +1,25 @@
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D 
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--inputfile",type=str)
-parser.add_argument("--show",type=int,choices=[0,1],default=1)
-parser.add_argument("--depth",type=int,default=1)
-parser.add_argument("--save",type=str,default="")
+parser.add_argument("--inputfile", type=str)
+parser.add_argument("--show", type=int, choices=[0, 1], default=1)
+parser.add_argument("--depth", type=int, default=1)
+parser.add_argument("--save", type=str, default="")
 
 args = parser.parse_args()
 inputfile = args.inputfile
 depth = args.depth
 
 # First Data
-data = (pd.read_csv(inputfile,header=None)).T
+data = (pd.read_csv(inputfile, header=None)).T
 header = data.iloc[0]
 print(header)
-data =data[1:]
+data = data[1:]
 data.columns = header
 
 
@@ -31,14 +31,18 @@ norm = colors.Normalize(vmin=min(data[str(depth)]), vmax=max(data[str(depth)]))
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-ax.scatter(data["x_0"].tolist(), data["x_1"].tolist(), data["x_2"].tolist(),c=colormap(norm(data[str(depth)].tolist())), marker='o')
-
+if "x_2" in data.columns:
+    ax.scatter(data["x_0"].tolist(), data["x_1"].tolist(), data["x_2"].tolist(
+    ), c=colormap(norm(data[str(depth)].tolist())), marker='o')
+else:
+    ax.scatter(data["x_0"].tolist(), data["x_1"].tolist(),
+               c=colormap(norm(data[str(depth)].tolist())), marker='o')
 
 # Output
 if args.show:
     plt.show()
 
 
-if args.save!="":
-    fig.savefig(args.save,bbox_inches = 'tight',
-    pad_inches = 0)
+if args.save != "":
+    fig.savefig(args.save, bbox_inches='tight',
+                pad_inches=0)
