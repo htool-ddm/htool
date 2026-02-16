a7a722ce403ef7176d9707b38537fa4acf7d68b3
Pierre Marchand <pierre.jimthefrench@gmail.com>
2026-02-16 17:14:30 +0100

    add theia

--- include/htool/hmatrix/lrmat/interpolation.hpp
+++ include/htool/hmatrix/lrmat/interpolation.hpp
@@ -5,67 +5,45 @@
 #include "../../wrappers/wrapper_blas.hpp"                      // for Blas
 #include "../../wrappers/wrapper_lapack.hpp"                    // for Blas
 #include "lrmat.hpp"
+#include <functional>
 #include <map>
 #include <span>
 
+#if defined(__clang__)
+#    pragma clang diagnostic push
+#    pragma clang diagnostic ignored "-Wold-style-cast"
+#elif defined(__GNUC__) || defined(__GNUG__)
+#    pragma GCC diagnostic push
+#    pragma GCC diagnostic ignored "-Wold-style-cast"
+#endif
+
+#include "theia.hpp"
+
+#if defined(__clang__)
+#    pragma clang diagnostic pop
+#elif defined(__GNUC__) || defined(__GNUG__)
+#    pragma GCC diagnostic pop
+#endif
+
 namespace htool {
 
-inline double cheb_node(int k, int L) { return std::cos((double)(2 * k + 1) / (double)(2 * L) * M_PI); }
-inline double T(double x, int k) { return cos((double)(k)*acos(x)); }
-inline void getTr(int L, double *Tr) {
-    for (int k = 0; k < L; k++) {
-        double r = cheb_node(k, L);
-        for (int i = 0; i < L; i++) {
-            (Tr + k * L)[i] = T(r, i);
-        }
-    }
-}
-inline void getTs(double u, int L, double *Tu) {
-    for (int i = 0; i < L; i++) {
-        Tu[i] = T(u, i);
-    }
-}
-inline void C1D(double *Tx, double *Tr, int L, double *S) {
-    for (int k = 0; k < L; k++) {
-        double res  = 1.;
-        double *Trk = Tr + k * L;
-        for (int j = 1; j < L; j++) {
-            res += 2. * Tx[j] * Trk[j];
-        }
-        S[k] = res / (double)(L);
-    }
-}
-inline void part_to_cheb(double x, double y, double z, double *Mu, int Lc) {
-    double *Cx = new double[Lc];
-    double *Cy = new double[Lc];
-    double *Cz = new double[Lc];
-    for (int i = 0; i < Lc; i++) {
-        Cx[i] = C1D(i, x, Lc);
-        Cy[i] = C1D(i, y, Lc);
-        Cz[i] = C1D(i, z, Lc);
-    }
-    for (int i = 0; i < Lc; i++) {
-        for (int j = 0; j < Lc; j++) {
-            for (int k = 0; k < Lc; k++) {
-                Mu[i * Lc * Lc + j * Lc + k] = Cx[i] * Cy[j] * Cz[k];
-            }
-        }
-    }
-}
-
 template <typename CoefficientPrecision, typename CoordinatePrecision, int dimension>
-class IBcomp final : public VirtualInternalLowRankGenerator<CoefficientPrecision> {
+class HCA final : public VirtualInternalLowRankGenerator<CoefficientPrecision> {
 
   private:
+    std::function<void(std::array<CoordinatePrecision, dimension> *, int, std::array<CoordinatePrecision, dimension> *, int, CoefficientPrecision *)> m_kernel;
     const int *m_target_permutation;
     const int *m_source_permutation;
-    std::function<CoefficientPrecision(CoordinatePrecision *, CoordinatePrecision *)> m_kernel;
-    const std::map<int, std::vector<std::pair<int, CoordinatePrecision>>> &
-        m_target_dof_to_elts;
-    const std::map<int, std::vector<std::pair<int, CoordinatePrecision>>> &m_source_dof_to_elts;
+    const CoordinatePrecision *m_target_points;
+    const CoordinatePrecision *m_source_points;
+    // const std::map<int, std::vector<std::pair<int, CoordinatePrecision>>> &
+    //     m_target_dof_to_elts;
+    // const std::map<int, std::vector<std::pair<int, CoordinatePrecision>>> &m_source_dof_to_elts;
 
   public:
-    IBcomp(std::function<CoefficientPrecision(CoordinatePrecision *, CoordinatePrecision *)> kernel, const int *target_permutation, const std::map<int, std::vector<std::pair<int, CoordinatePrecision>>> &target_dof_to_elts, const int *source_permutation, const std::map<int, std::vector<std::pair<int, CoordinatePrecision>>> &source_dof_to_elts) : m_kernel(kernel), m_target_permutation(target_permutation), m_target_dof_to_elts(target_dof_to_elts), m_source_permutation(source_permutation), m_source_dof_to_elts(source_dof_to_elts) {}
+    // HCA(std::function<CoefficientPrecision(std::array<CoordinatePrecision, 3>, std::array<CoordinatePrecision, 3>)> kernel, const int *target_permutation, const std::map<int, std::vector<std::pair<int, CoordinatePrecision>>> &target_dof_to_elts, const int *source_permutation, const std::map<int, std::vector<std::pair<int, CoordinatePrecision>>> &source_dof_to_elts) : m_kernel(kernel), m_target_permutation(target_permutation), m_target_dof_to_elts(target_dof_to_elts), m_source_permutation(source_permutation), m_source_dof_to_elts(source_dof_to_elts) {}
+
+    HCA(std::function<void(std::array<CoordinatePrecision, dimension> *, int, std::array<CoordinatePrecision, dimension> *, int, CoefficientPrecision *)> kernel, const int *target_permutation, const int *source_permutation) : m_kernel(kernel), m_target_permutation(target_permutation), m_source_permutation(source_permutation) {}
 
     virtual bool copy_low_rank_approximation(int M, int N, int row_offset, int col_offset, LowRankMatrix<CoefficientPrecision> &lrmat) const override {
         return copy_low_rank_approximation(M, N, m_target_permutation + row_offset, m_source_permutation + col_offset, lrmat);
@@ -75,7 +53,6 @@ class IBcomp final : public VirtualInternalLowRankGenerator<CoefficientPrecision
         return copy_low_rank_approximation(M, N, m_target_permutation + row_offset, m_source_permutation + col_offset, reqrank, lrmat);
     }
 
-    // C style
     bool copy_low_rank_approximation(int M, int N, const int *rows, const int *cols, LowRankMatrix<CoefficientPrecision> &lrmat) const {
         std::array<CoordinatePrecision, 2 * dimension> target_box;
         std::array<CoordinatePrecision, 2 * dimension> source_box;
@@ -85,25 +62,50 @@ class IBcomp final : public VirtualInternalLowRankGenerator<CoefficientPrecision
             source_box[2 * dim + 0] = std::numeric_limits<double>::min();
             source_box[2 * dim + 1] = std::numeric_limits<double>::max();
         }
-        for (int target_index : std::span<int>(rows, N)) {
-            auto &target_elts = m_target_dof_to_elts[target_index];
-            for (auto [_, elt] : target_elts) {
-                for (int dim = 0; dim < dimension; dim++) {
-                    target_box[2 * dim + 0] = std::max(target_box[2 * dim + 0], elt[dim]);
-                    target_box[2 * dim + 1] = std::min(target_box[2 * dim + 1], elt[dim]);
-                }
+        for (int i = 0; i < M; i++) {
+            for (int dim = 0; dim < dimension; dim++) {
+                target_box[2 * dim + 0] = std::max(target_box[2 * dim + 0], m_target_points[rows[i] * dim + dim]);
+                target_box[2 * dim + 1] = std::min(target_box[2 * dim + 1], m_target_points[rows[i] * dim + dim]);
             }
         }
-        for (int source_index : std::span<int>(cols, N)) {
-            auto &source_elts = m_source_dof_to_elts[source_index];
-            for (auto [_, elt] : source_elts) {
-                for (int dim = 0; dim < dimension; dim++) {
-                    source_box[2 * dim + 0] = std::max(source_box[2 * dim + 0], elt[dim]);
-                    source_box[2 * dim + 1] = std::min(source_box[2 * dim + 1], elt[dim]);
-                }
+        for (int j = 0; j < N; j++) {
+            for (int dim = 0; dim < dimension; dim++) {
+                source_box[2 * dim + 0] = std::max(source_box[2 * dim + 0], m_source_points[cols[j] * dim + dim]);
+                source_box[2 * dim + 1] = std::min(source_box[2 * dim + 1], m_source_points[cols[j] * dim + dim]);
             }
         }
+
+        return true;
     }
+    // // C style
+    // bool copy_low_rank_approximation(int M, int N, const int *rows, const int *cols, LowRankMatrix<CoefficientPrecision> &lrmat) const {
+    //     std::array<CoordinatePrecision, 2 * dimension> target_box;
+    //     std::array<CoordinatePrecision, 2 * dimension> source_box;
+    //     for (int dim = 0; dim < dimension; dim++) {
+    //         target_box[2 * dim + 0] = std::numeric_limits<double>::min();
+    //         target_box[2 * dim + 1] = std::numeric_limits<double>::max();
+    //         source_box[2 * dim + 0] = std::numeric_limits<double>::min();
+    //         source_box[2 * dim + 1] = std::numeric_limits<double>::max();
+    //     }
+    //     for (int target_index : std::span<int>(rows, N)) {
+    //         auto &target_elts = m_target_dof_to_elts[target_index];
+    //         for (auto [_, elt] : target_elts) {
+    //             for (int dim = 0; dim < dimension; dim++) {
+    //                 target_box[2 * dim + 0] = std::max(target_box[2 * dim + 0], elt[dim]);
+    //                 target_box[2 * dim + 1] = std::min(target_box[2 * dim + 1], elt[dim]);
+    //             }
+    //         }
+    //     }
+    //     for (int source_index : std::span<int>(cols, N)) {
+    //         auto &source_elts = m_source_dof_to_elts[source_index];
+    //         for (auto [_, elt] : source_elts) {
+    //             for (int dim = 0; dim < dimension; dim++) {
+    //                 source_box[2 * dim + 0] = std::max(source_box[2 * dim + 0], elt[dim]);
+    //                 source_box[2 * dim + 1] = std::min(source_box[2 * dim + 1], elt[dim]);
+    //             }
+    //         }
+    //     }
+    // }
 
     bool copy_low_rank_approximation(int M, int N, const int *rows, const int *cols, int reqrank, LowRankMatrix<CoefficientPrecision> &lrmat) const {}
 };
---
+++ tests/functional_tests/hmatrix/lrmat/lrmat_build/test_lrmat_build_HCA.cpp
@@ -0,0 +1,76 @@
+#include "../test_lrmat_build.hpp"                        // for test_lrmat
+#include <algorithm>                                      // for max, copy
+#include <cmath>                                          // for pow, sqrt
+#include <htool/clustering/cluster_node.hpp>              // for Cluster
+#include <htool/clustering/tree_builder/tree_builder.hpp> // for Cluster...
+#include <htool/hmatrix/interfaces/virtual_generator.hpp> // for Generat...
+#include <htool/hmatrix/lrmat/interpolation.hpp>          // for HCA
+#include <htool/hmatrix/lrmat/lrmat.hpp>                  // for LowRank...
+#include <htool/testing/generator_test.hpp>               // for Generat...
+#include <htool/testing/geometry.hpp>                     // for create_...
+#include <iostream>                                       // for basic_o...
+#include <utility>                                        // for pair
+#include <vector>                                         // for vector
+
+using namespace std;
+using namespace htool;
+
+int main(int, char *[]) {
+
+    const int ndistance = 4;
+    double distance[ndistance];
+    distance[0] = 15;
+    distance[1] = 20;
+    distance[2] = 30;
+    distance[3] = 40;
+
+    double epsilon = 0.0001;
+
+    int nr = 500;
+    int nc = 100;
+    std::vector<double> xt(3 * nr);
+    std::vector<double> xs(3 * nc);
+    std::vector<int> tabt(500);
+    std::vector<int> tabs(100);
+    bool test = 0;
+    for (int idist = 0; idist < ndistance; idist++) {
+
+        create_disk(3, 0., nr, xt.data());
+        create_disk(3, distance[idist], nc, xs.data());
+
+        ClusterTreeBuilder<double> recursive_build_strategy;
+
+        Cluster<double> t = recursive_build_strategy.create_cluster_tree(nr, 3, xt.data(), 2, 2);
+        Cluster<double> s = recursive_build_strategy.create_cluster_tree(nc, 3, xt.data(), 2, 2);
+
+        GeneratorTestDouble A_in_user_numbering(3, xt, xs);
+        InternalGeneratorWithPermutation<double> A(A_in_user_numbering, t.get_permutation().data(), s.get_permutation().data());
+
+        auto kernel = std::function([](std::array<double, 3> *target_points, int Nx, std::array<double, 3> *source_points, int Ny, double *mat) {
+            for (int i = 0; i < Nx; i++) {
+                for (int j = 0; j < Ny; j++) {
+                    mat[i + j * Nx] = 1. / (4 * M_PI * std::sqrt(std::inner_product(target_points[i].begin(), target_points[i].end(), source_points[i].begin(), double(0), std::plus<double>(), [](double u, double v) { return (u - v) * (u - v); })));
+                }
+            }
+        });
+
+        // partialACA fixed rank
+        int reqrank_max = 10;
+        HCA<double, double, 3> compressor(kernel, t.get_permutation().data(), s.get_permutation().data());
+
+        LowRankMatrix<double> A_partialACA_fixed(t.get_size(), s.get_size(), reqrank_max, epsilon);
+        compressor.copy_low_rank_approximation(t.get_size(), s.get_size(), t.get_offset(), s.get_offset(), reqrank_max, A_partialACA_fixed);
+
+        // ACA automatic building
+        LowRankMatrix<double> A_partialACA(t.get_size(), s.get_size(), epsilon);
+        compressor.copy_low_rank_approximation(t.get_size(), s.get_size(), t.get_offset(), s.get_offset(), A_partialACA);
+
+        std::pair<double, double> fixed_compression_interval(0.87, 0.89);
+        std::pair<double, double> auto_compression_interval(0.93, 0.96);
+        test = test || (test_lrmat(t, s, A, A_partialACA_fixed, A_partialACA, fixed_compression_interval, auto_compression_interval));
+    }
+
+    cout << "test : " << test << endl;
+
+    return test;
+}
